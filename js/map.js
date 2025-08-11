6c92522096ad363710279a245b7bb4e04833d6b3
Payam Chamani <pchamani@nizek.com>
2025-08-11 19:26:18 +0330

    new maps

--- js/map.js
+++ js/map.js
@@ -1,131 +1,33 @@
-function initialize() {
-      var mapOptions = {
-        zoom: 10,
-        scrollwheel: false,
-                styles: [{
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#e9e9e9"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#f5f5f5"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#ffffff"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#ffffff"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#ffffff"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#ffffff"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#f5f5f5"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "featureType": "poi.park",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#d5d5d5"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#f8f8f8"
-            }, {
-                "lightness": 25
-            }]
-        }, {
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#222222"
-            }, {
-                "lightness": 30
-            }]
-        }, {
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#f5f5f5"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#fefefe"
-            }, {
-                "lightness": 10
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#fefefe"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }],
-        center: new google.maps.LatLng(40.712775, -74.005973)
-      };
-      var map = new google.maps.Map(document.getElementById('map'),
-          mapOptions);
-      var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        icon: 'images/locating.png',
-        map: map
-      });
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);   
+// Initialize Leaflet map using OpenStreetMap tiles
+(function () {
+  var centerLat = 40.712775;
+  var centerLng = -74.005973;
+  var zoomLevel = 10;
+
+  function initializeLeaflet() {
+    if (!document.getElementById('map')) return;
+
+    var map = L.map('map', {
+      scrollWheelZoom: false
+    }).setView([centerLat, centerLng], zoomLevel);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    var icon = L.icon({
+      iconUrl: 'images/locating.png',
+      iconSize: [32, 32],
+      iconAnchor: [16, 32]
+    });
+
+    L.marker([centerLat, centerLng], { icon: icon }).addTo(map);
+  }
+
+  if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    initializeLeaflet();
+  } else {
+    window.addEventListener('load', initializeLeaflet);
+  }
+})();
